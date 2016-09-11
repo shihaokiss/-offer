4ce692070607ae229f2e0517fb439cca554c24e6
shihaokiss <shihao_kiss@outlook.com>
2016-09-12 00:12:19 +0800

    查找和为偶数的素数对

---
+++ 查找和为偶数的素数对.h
@@ -0,0 +1,34 @@
+/*寻找素数*/
+int* findPrime(int num)
+{
+	int* arr = new int[num + 1]();
+	for (int i = 2; i < num; ++i)
+	{
+		if (arr[i] == 1)
+			continue;
+		for (int j = i + i; j <= num; j += i)
+		{
+			arr[j] = 1;
+		}
+	}
+
+	return arr;
+}
+
+void main()
+{
+	int* ptr = findPrime(1000);
+	int num,count = 0;
+	cin >> num;
+	for (int i = 1; i < num; ++i)
+	{
+		if (ptr[i] == 0 && ptr[num - i] == 0)
+		{
+			cout << i << "+" << num - i << "=" << num << endl;
+			count++;
+		}
+	}
+	if (ptr[num / 2] == 0)
+		++count;
+	cout <<"共 "<< count/2<<" 对" << endl;
+}
